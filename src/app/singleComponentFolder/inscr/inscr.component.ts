a8fa12c96ddf1ee3ca8e04c051f78a137e9f4631
mathisdelehouzee <mathdel@outlook.com>
2019-11-15 14:46:49 +0100

    ajout du lien entre node et angular

--- src/app/singleComponentFolder/inscr/inscr.component.ts
+++ src/app/singleComponentFolder/inscr/inscr.component.ts
@@ -1,24 +1,51 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {Inscription} from './inscr.interface';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
 
 @Component({
   selector: 'app-inscr',
   templateUrl: './inscr.component.html',
-  styleUrls: ['./inscr.component.css','../../app.component.css']
+  styleUrls: ['./inscr.component.css']
 })
+
+@Injectable()
 export class InscrComponent implements OnInit {
+  inscription: Inscription = {
+    id_utilisateur: null,
+    adresse_mail: '',
+    mot_passe: '',
+    verification_mot_passe: ''
+  };
 
-  @Input() inscrMail: string;
-  @Input() inscrMdp: string;
-  @Input() inscrVerifMdp: string;
-
-  constructor(private router: Router) { }
-
+  dataNode: Inscription[];
+  constructor(private router: Router, private http: HttpClient) {
+  }
   ngOnInit() {
+    this.http
+        .get(`http://localhost:8080/`)
+        .subscribe((data: Inscription[]) => {
+          this.dataNode = data;
+          console.log(data);
+        });
   }
 
-  inscrire(){
+  inscrire(data: Inscription) {
     this.router.navigate(['accueil']);
+    console.log('adresse mail: ' + data.adresse_mail);
+    console.log('mot de passe: ' + data.mot_passe);
+
+    this.http.post(`http://localhost:8080/`, data)
+      .subscribe(
+        res => {
+          console.log(res);
+        },
+        err => {
+          console.log('Error occured:' , err);
+        }
+      );
   }
 
 }
