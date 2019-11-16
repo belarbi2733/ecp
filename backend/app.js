2c7120372beaf6e25c26b1cfd7a6216765ba172b
rolandus10 <rolandguy18@gmail.com>
2019-11-16 10:28:04 +0100

    node code

--- backend/app.js
+++ backend/app.js
@@ -18,22 +18,22 @@ var bodyParser = require('body-parser');
 
 const { Pool, Client } = require('pg')
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'easycarpool',
-    password: '1397',
-    port: 5432,
+  user: 'postgres',
+  host: 'localhost',
+  database: 'Projet_easyCarPool',
+  password: 'mac',
+  port: 5432,
 })
 // pool.query('SELECT * FROM utilisateur', (err, res) => {
 //     console.log(err, res)
 //     pool.end()
 // })
 const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'easycarpool',
-    password: '1397',
-    port: 5432,
+  user: 'postgres',
+  host: 'localhost',
+  database: 'Projet_easyCarPool',
+  password: 'mac',
+  port: 5432,
 })
 
 
@@ -42,11 +42,11 @@ const client = new Client({
 
 console.log(user);
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    req.header("Access-Control-Allow-Origin", "*");
-    req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  req.header("Access-Control-Allow-Origin", "*");
+  req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 app.use(bodyParser.json());
@@ -54,34 +54,34 @@ app.use(bodyParser.json());
 app.get('/',function (req,res) {
 
 
-    var inscription = [{
-        id_utilisateur: 'test_id',
-        adresse_mail: 'test_adress',
-        mot_passe: 'test_mot',
-        verification_mot_passe: 'test_mot',
-    }];
+  var inscription = [{
+    id_utilisateur: 'test_id',
+    adresse_mail: 'test_adress',
+    mot_passe: 'test_mot',
+    verification_mot_passe: 'test_mot',
+  }];
 
-    res.json(inscription);
+  res.json(inscription);
 
 })
 
-app.post('/', function (req, res) {
-    // const query = {
-    //     // give the query a unique name
-    //     name: 'insert-user',
-    //     text: 'Insert INTO utilisateurs(adresse_mail, mot_passe, nom, prenom, datenaissance) VALUES(?, ?, ?, ?, ?)',
-    //     values: [(req.body).adresse_mail,
-    //         (req.body).mot_passe, 'guy', 'Roland', 1995-10-10],
-    // }
-    //
-    // pool.query('Insert INTO utilisateurs(adresse_mail, mot_passe, nom, prenom, datenaissance) VALUES(?, ?, ?, ?, ?)'
-    // values: [(req.body).adresse_mail,
-    //     (req.body).mot_passe, 'guy', 'Roland', 1995-10-10]', (err,res) => {
-    //         console.log(res)
-    //     });
-    // console.log('message angular recu!'+ req.body.adresse_mail);
-    pool.connect()
-    pool.query("Insert into USER (mail, note, nom, prenom) values ($1,$2,$3,$4)",[req.body.adresse_mail,req.body.mot_passe,"qdzfaz","qscqcq"],console.log("Insert done"));
+app.post('/inscription', function (req, res) {
+  // const query = {
+  //     // give the query a unique name
+  //     name: 'insert-user',
+  //     text: 'Insert INTO utilisateurs(adresse_mail, mot_passe, nom, prenom, datenaissance) VALUES(?, ?, ?, ?, ?)',
+  //     values: [(req.body).adresse_mail,
+  //         (req.body).mot_passe, 'guy', 'Roland', 1995-10-10],
+  // }
+  //
+  // pool.query('Insert INTO utilisateurs(adresse_mail, mot_passe, nom, prenom, datenaissance) VALUES(?, ?, ?, ?, ?)'
+  // values: [(req.body).adresse_mail,
+  //     (req.body).mot_passe, 'guy', 'Roland', 1995-10-10]', (err,res) => {
+  //         console.log(res)
+  //     });
+  // console.log('message angular recu!'+ req.body.adresse_mail);
+  pool.connect()
+  pool.query("Insert into utilisateurs (adresse_mail, mot_passe, nom, prenom) values ($1,$2,$3,$4)",[req.body.adresse_mail,req.body.mot_passe,"qdzfaz","qscqcq"],console.log("Insert done"));
 
 })
 
