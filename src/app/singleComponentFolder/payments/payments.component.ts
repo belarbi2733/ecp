fdf6a5fadfc71a869e9af4e16ecefa8e430eb1e2
GreatToubib <basilburleon@hotmail.com>
2019-11-16 21:34:37 +0100

    css basique terminé

--- src/app/singleComponentFolder/payments/payments.component.ts
+++ src/app/singleComponentFolder/payments/payments.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-payments',
   templateUrl: './payments.component.html',
-  styleUrls: ['./payments.component.css']
+  styleUrls: ['./payments.component.css',  '../../app.component.css']
 })
 export class PaymentsComponent implements OnInit {
 
