936c3c55adec1ed7990afdf7bd76f6361db5a808
MargotLiagre <44575578+MargotLiagre@users.noreply.github.com>
2019-11-19 13:49:03 +0100

    Update inscr.component.ts

--- src/app/singleComponentFolder/inscr/inscr.component.ts
+++ src/app/singleComponentFolder/inscr/inscr.component.ts
@@ -1,38 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {Inscription} from './inscr.interface';
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import {InscrService} from './inscr.service';
-
 
 @Component({
   selector: 'app-inscr',
   templateUrl: './inscr.component.html',
-  styleUrls: ['./inscr.component.css']
+  styleUrls: ['./inscr.component.css','../../app.component.css']
 })
-
-@Injectable()
 export class InscrComponent implements OnInit {
-  inscription: Inscription = {
-    id_utilisateur: null,
-    adresse_mail: '',
-    mot_passe: '',
-    verification_mot_passe: ''
-  };
 
-  dataNode: Inscription[];
-  constructor(private inscrService: InscrService, private router: Router, private http: HttpClient) {
-  }
+  @Input() inscrMail: string;
+  @Input() inscrMdp: string;
+  @Input() inscrVerifMdp: string;
+
+  constructor(private router: Router) { }
+
   ngOnInit() {
   }
 
-  inscrire(data: Inscription) {
-
-    this.inscrService.inscription(data);
+  inscrire(){
     this.router.navigate(['accueil']);
-    console.log('adresse mail: ' + data.adresse_mail);
-    console.log('mot de passe: ' + data.mot_passe);
   }
 
 }
