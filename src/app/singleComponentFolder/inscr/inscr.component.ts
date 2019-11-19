d8141a028fe285f3a83c9d02b6324ec6d4a21f2a
MargotLiagre <171245@umons.ac.be>
2019-11-19 14:30:32 +0100

    Correction du dossier inscr (reprise de l'ancienne version) + mise à jour de axios 0.18.0 à 0.18.1

--- src/app/singleComponentFolder/inscr/inscr.component.ts
+++ src/app/singleComponentFolder/inscr/inscr.component.ts
@@ -1,29 +1,51 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {Inscription} from './inscr.interface';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {InscrService} from './inscr.service';
 
 
 @Component({
   selector: 'app-inscr',
   templateUrl: './inscr.component.html',
-  styleUrls: ['./inscr.component.css']
+  styleUrls: ['./inscr.component.css' , '../../app.component.css']
 })
+
+@Injectable()
 export class InscrComponent implements OnInit {
+  inscription: Inscription = {
+    id_utilisateur: null,
+    adresse_mail: '',
+    mot_passe: '',
+    verification_mot_passe: ''
+  };
+
   dataNode: Inscription[];
-  constructor(private inscrService: InscrService, private router: Router, private http: HttpClient) {
+  constructor(private router: Router, private http: HttpClient) {
   }
   ngOnInit() {
+    this.http
+        .get(`http://localhost:8080/`)
+        .subscribe((data: Inscription[]) => {
+          this.dataNode = data;
+          console.log(data);
+        });
   }
 
   inscrire(data: Inscription) {
-
-    this.inscrService.inscription(data);
     this.router.navigate(['accueil']);
     console.log('adresse mail: ' + data.adresse_mail);
     console.log('mot de passe: ' + data.mot_passe);
+
+    this.http.post(`http://localhost:8080/`, data)
+      .subscribe(
+        res => {
+          console.log(res);
+        },
+        err => {
+          console.log('Error occured:' , err);
+        }
+      );
   }
 
 }
