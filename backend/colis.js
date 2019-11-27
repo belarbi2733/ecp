e50f16bfcee90acf8315839006357d9316323eec
Kunzyy <44604068+Kunzyy@users.noreply.github.com>
2019-11-28 00:25:07 +0100

    Update addColis for working + generate Trajet after addColis

--- backend/colis.js
+++ backend/colis.js
@@ -20,7 +20,8 @@ let Colis = {
   },
 
   generateTrajet: function(colis, depart, arrivee, callback) {
-    return db.query('INSERT INTO trajet (id_user,id_colis,depart,arrivee,nbre_places) VALUES (SELECT id_user FROM colis WHERE id_user = $1,SELECT id FROM colis WHERE nom = $2 AND id_user = $3,$4,$5,$6)',[colis.idUser,colis.nom,colis.idUser,depart,arrivee,0],callback);
+    console.log("Generate Trajet en cours...");
+    return db.query('INSERT INTO trajet (id_user,id_colis,depart,arrivee,nbre_places) VALUES ($1,$2,$3,$4,$5)',[colis.idUser,1,depart,arrivee,0],callback);
   }
 };
 
