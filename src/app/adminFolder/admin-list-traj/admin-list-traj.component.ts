b84302b18dcd61ecb1b99c5a55aeb4b0c551e09d
mathisdelehouzee <mathdel@outlook.com>
2019-11-30 19:24:14 +0100

    Display trajcts on admin board

--- src/app/adminFolder/admin-list-traj/admin-list-traj.component.ts
+++ src/app/adminFolder/admin-list-traj/admin-list-traj.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
+import {DataTrajService} from '../../services/admin-list-traj.service';
+
+import {DataTrajet} from '../../singleComponentFolder/add-trajet/add-trajet.interface';
+import {DispListTraj} from './dispadmintraj.interface';
+
+
 @Component({
   selector: 'app-admin-list-traj',
   templateUrl: './admin-list-traj.component.html',
   styleUrls: ['./admin-list-traj.component.css']
 })
+
+
+
 export class AdminListTrajComponent implements OnInit {
+  adminListTraj: DispListTraj = {
+    idUser: null,
+    depart: '',
+    arrivee: '',
+    nbrePlaces: null
+  };
 
-  constructor() { }
+  error: string;
 
-  ngOnInit() {
+  constructor(private  dataTrajService: DataTrajService) {
+    this.adminListTraj.idUser = JSON.parse(localStorage.getItem('idUser')).id;
   }
 
+  ngOnInit() {
+    this.dataTrajService.dispAdminTraj(this.adminListTraj)
+      .then((dataTrajet: DataTrajet) => {
+        this.adminListTraj.arrivee = dataTrajet.arrivee;
+        this.adminListTraj.depart = dataTrajet.depart;
+        this.adminListTraj.idUser = dataTrajet.id_utilisateur;
+        this.adminListTraj.nbrePlaces = dataTrajet.places;
+      })
+      .catch(() => {
+        console.log('Error in getUserDataById');
+      });
+  }
 }
+
