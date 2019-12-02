21db245c5ff66d7508449775d58bf427556a2f69
Kunzyy <44604068+Kunzyy@users.noreply.github.com>
2019-12-02 13:50:54 +0100

    Fix a little error

--- backend/voiture.js
+++ backend/voiture.js
@@ -2,6 +2,12 @@ let db = require("./db.js");
 
 let Voiture = {
 
+  addVoiture: function (voiture,callback) {
+    console.log('addVoiture: idUser = ', voiture.idUser);
+    db.query('INSERT INTO voiture ()');
+  }
+
+
 };
 
 
