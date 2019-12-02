5af764748c14d011398a8ad886e286c698aa7d7c
mathisdelehouzee <mathdel@outlook.com>
2019-12-02 23:31:20 +0100

    Display User in admin board sucess

--- src/app/adminFolder/admin-list-ut/admin-list-ut.component.ts
+++ src/app/adminFolder/admin-list-ut/admin-list-ut.component.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {AdminListUtService} from '../../services/admin-list-user.service';
+import {AdminListUtInterface} from './admin-list-ut.interface';
 
 @Component({
   selector: 'app-admin-list-ut',
   templateUrl: './admin-list-ut.component.html',
   styleUrls: ['./admin-list-ut.component.css']
 })
-export class AdminListUtComponent implements OnInit {
 
-  constructor(private router: Router) { }
+export class AdminListUtComponent implements OnInit {
+  adminListUtInterface: AdminListUtInterface = {
+    nom: 'testnameadminut',
+    prenom: 'testprenomadminut',
+    nbre_traj: 5
+  };
+
+  constructor(private adminListUtService: AdminListUtService) {
+  }
 
   ngOnInit() {
+    this.adminListUtService.getAllUser(this.adminListUtInterface)
+      .then((adminListUtInterface: AdminListUtInterface) => {
+        this.adminListUtInterface.nom = adminListUtInterface.nom;
+        this.adminListUtInterface.prenom = adminListUtInterface.prenom;
+        this.adminListUtInterface.nbre_traj = adminListUtInterface.nbre_traj;
+      })
+      .catch(() => {
+        console.log('Error in getUserDataById');
+      });
   }
-
-  suspendre(){
-    this.router.navigate(['admin-list-ut']);
-  }
-
 }
