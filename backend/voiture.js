f51619ea759138bd0729985ef781cc425e673580
Kunzyy <44604068+Kunzyy@users.noreply.github.com>
2019-12-02 14:51:36 +0100

    Fix a little error

--- backend/voiture.js
+++ backend/voiture.js
@@ -4,7 +4,7 @@ let Voiture = {
 
   addVoiture: function (voiture,callback) {
     console.log('addVoiture: idUser = ', voiture.idUser);
-    db.query('INSERT INTO voiture ()');
+    db.query('INSERT INTO voiture (id_user, nom_marque, nom_modele, nbre_places, coffre)',[voiture.idUser, voiture.modele, voiture]);
   }
 
 
