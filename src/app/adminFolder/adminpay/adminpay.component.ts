e45ac95eae726234323c64de289da77681ecf53b
MargotLiagre <171245@umons.ac.be>
2019-12-06 16:42:32 +0100

    Ajout de gestion des payements

---
+++ src/app/adminFolder/adminpay/adminpay.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adminpay',
+  templateUrl: './adminpay.component.html',
+  styleUrls: ['./adminpay.component.css']
+})
+export class AdminpayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  PayementEffect(){
+    
+  }
+
+}
