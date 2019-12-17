0aa85231b3cb4ab5ce870de530751a9c739c5899
mathd <mathdel@outlook.com>
2019-12-17 15:04:33 +0100

    Dynamique display of user only for necessary informations

--- src/app/adminFolder/admin-list-ut/admin-list-ut.component.ts
+++ src/app/adminFolder/admin-list-ut/admin-list-ut.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import {AdminListUtService} from '../../services/admin-list-user.service';
 import {AdminListUtInterface} from './admin-list-ut.interface';
 
+
 @Component({
   selector: 'app-admin-list-ut',
   templateUrl: './admin-list-ut.component.html',
-  styleUrls: ['./admin-list-ut.component.css','../../app.component.css']
+  styleUrls: ['./admin-list-ut.component.css']
 })
+
 
 export class AdminListUtComponent implements OnInit {
   adminListUtInterface: AdminListUtInterface = {
@@ -15,15 +17,14 @@ export class AdminListUtComponent implements OnInit {
     id: null
   };
 
+
   constructor(private adminListUtService: AdminListUtService) {
   }
 
   ngOnInit() {
     this.adminListUtService.getAllUser(this.adminListUtInterface)
       .then((adminListUtInterface: AdminListUtInterface) => {
-        this.adminListUtInterface.nom = adminListUtInterface.nom;
-        this.adminListUtInterface.prenom = adminListUtInterface.prenom;
-        this.adminListUtInterface.id = adminListUtInterface.id;
+        this.adminListUtInterface = adminListUtInterface;
       })
       .catch(() => {
         console.log('Error in getUserDataById');
