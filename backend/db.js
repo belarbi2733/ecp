0ed4edcea029e096f1d6f36eda33362272f85d02
Thomas Guily <ThomasGuily@users.magellan.fpms.ac.be>
2020-01-02 20:49:36 +0100

    Validation effective en local via Mailtrap

--- backend/db.js
+++ backend/db.js
@@ -2,8 +2,8 @@ let pg = require("pg");
 
 let config = {
   user: 'postgres',
-  database: 'ECP',
-  password: '1397', // !!!!!!!!!! Il faut mettre votre mdp de votre database sinon ca fonctionne pas
+  database: 'ecp',
+  password: 'Bonus', // !!!!!!!!!! Il faut mettre votre mdp de votre database sinon ca fonctionne pas
   port: 5432
 };
 
