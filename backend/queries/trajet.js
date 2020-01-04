8d00beb981c12161c4681090ea88e693640db782
Kunzyy <44604068+Kunzyy@users.noreply.github.com>
2020-01-04 23:11:41 +0100

    Date naissance ok

--- backend/queries/trajet.js
+++ backend/queries/trajet.js
@@ -34,21 +34,6 @@ let Trajet = {
   getAllTrajets: function(callback){
     return db.query('SELECT trajet.id, trajet.depart_address, trajet.departure_time, trajet.book_places, trajet.prix, trajet.statut, utilisateur.paypal  FROM trajet INNER JOIN utilisateur ON utilisateur.id=trajet.id_user ORDER BY trajet.departure_time DESC', callback);
   },
-// WHERE trajet.statut >= 1 ORDER BY trajet.id DESC
-//   SELECT employee.LastName, employee.DepartmentID, department.DepartmentName
-// FROM employee
-// INNER JOIN department ON
-// employee.DepartmentID = department.DepartmentID;
-
-
-// id: result.rows[i].id,
-//           depart : result.rows[i].depart_address,
-//           arrivee : result.rows[i].arrivee_address,
-//           nbrePlaces : places,
-//           prix : result.rows[i].prix,
-//           colis : colisName,
-//           paypal : paypalAccount,
-//           statut : result.rows[i].statut
 
   getAllTrajEffec: function(callback){
     console.log("Get All Trajet effectué");
